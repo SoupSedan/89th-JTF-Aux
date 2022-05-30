e9383475b1050dc19e5dd4dc5866575a05698c27
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2022-05-29 16:36:31 -1000

    Updated Cyborgs Custom Gear

--- 89th_Armor/Data/Custom/config.cpp
+++ 89th_Armor/Data/Custom/config.cpp
@@ -407,6 +407,149 @@ class cfgWeapons
 			"89th_Armor\Data\Basic\ARD\89th_M52D_ARD_CO.paa"
 		};
 	};
+	//Cyborg Vest
+	class 89th_M52D_Cyborg: VES_M52D_Demolitions
+	{
+		dlc="FZ";
+		author="Vespade";
+		displayName="[89th] M52 (Cyborg) (WDL)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\WDL\89th_Soft_Vest_WDL.paa",
+			"89th_Armor\Data\Custom\cyborg\89th_M52_cyborg_WDL_CO.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52_WDL_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52D_WDL_CO.paa"};
+		class ItemInfo: VestItem
+		{
+			mass=75;
+			containerClass="Supply250";
+			uniformModel="\OPTRE_UNSC_Units\Army\armor";
+			vestType="Rebreather";
+			class HitpointsProtectionInfo
+			{
+				class Abdomen {armor=35; hitpointName="HitAbdomen"; passThrough=0.1;};
+				class Arms: Abdomen {hitpointName="HitArms";};
+				class Body: Abdomen {hitpointName="HitBody";};
+				class Chest: Abdomen {hitpointName="HitChest";};
+				class Diaphragm: Abdomen {hitpointName="HitDiaphragm";};
+				class Hands: Abdomen {hitpointName="HitHands";};
+				class Legs: Abdomen {hitpointName="HitLegs";};
+				class Neck: Abdomen {hitpointName="HitNeck";};
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+			\\	"A_ChestArmor",
+			\\	"A_KneesLeft",
+			\\	"A_KneesRight",
+			\\	"A_KneesMarLeft",
+			\\	"A_KneesMarRight",
+				"A_ODST",
+			\\	"A_ShinArmorLeft",
+			\\	"A_ShinArmorRight",
+				"A_TacPad",
+			\\	"A_ThighArmorLeft",
+			\\	"A_ThighArmorRight",
+			\\	"AS_BaseLeft",
+			\\	"AS_BaseRight",
+			\\	"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+				"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+				"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+			\\	"AS_SmallRight",
+			\\	"AP_Canteen",
+				"AP_GL",
+				"AP_Knife",
+				"AP_MGThigh",
+				"AP_AR",
+				"AP_BR",
+				"AP_Pack",
+			\\	"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+				"AP_Sniper",
+				"AP_Thigh",
+				"AP_Frag",
+				"AP_Smoke",
+			\\	"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};
+	class 89th_M52D_Cyborg_DES: 89th_M52D_cyborg
+	{
+		dlc="FZ";
+		author="Vespade";
+		displayName="[89th] M52 (Cyborg) (DES)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\DES\89th_Soft_Vest_DES.paa",
+			"89th_Armor\Data\Custom\cyborg\89th_M52_Cyborg_DES_CO.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52_DES_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52D_DES_CO.paa"
+		};
+	};
+	class 89th_M52D_Cyborg_URB: 89th_M52D_cyborg
+	{
+		dlc="FZ";
+		author="Vespade";
+		displayName="[89th] M52 (Cyborg) (URB)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\URB\89th_Soft_Vest_URB.paa",
+			"89th_Armor\Data\Custom\cyborg\89th_M52_cyborg_URB_CO.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52_URB_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52D_URB_CO.paa"
+		};
+	};
+	class 89th_M52D_Cyborg_SNW: 89th_M52D_cyborg
+	{
+		dlc="FZ";
+		author="Vespade";
+		displayName="[89th] M52 (Cyborg) (SNW)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\SNW\89th_Soft_Vest_SNW.paa",
+			"89th_Armor\Data\Custom\cyborg\89th_M52_cyborg_SNW_CO.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52_SNW_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52D_SNW_CO.paa"
+		};
+	};
+	class 89th_M52D_Cyborg_ARD: 89th_M52D_cyborg
+	{
+		dlc="FZ";
+		author="Vespade";
+		displayName="[89th] M52 (Cyborg) (ARD)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\ARD\89th_Soft_Vest_ARD.paa",
+			"89th_Armor\Data\Custom\cyborg\89th_M52_cyborg_ARD_CO.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52_ARD_Underlay_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52D_ARD_CO.paa"
+		};
+	};
 
 		//Jarvis Vest
 	class 89th_M52D_Jarvis: VES_M52D_Demolitions
@@ -6000,219 +6143,109 @@ class 89th_CH252D_Epsilon_URB: VES_CH252D
 			};
 		};
 	};
-		//Cyborg Helmet
-	class 89th_Marine_WDL_Cyborg_ECH252: VES_CH252_MAR_Vacuum
+	//Cyborg Helmet
+	class 89th_Marine_ARD_Cyborg_CH252: VES_CH252_MAR_Light
 	{
 		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (WDL)";
-		model="\optre_unsc_units\army\helmet";
 		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Cyborg)(ARD)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_WDL.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
-		};
-		class ItemInfo: ItemInfo
-		{
-			model="\optre_unsc_units\army\helmet";
-			hiddenSelectionsTextures[]=
-			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_WDL.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
-			};
-		};
-	};
-	class 89th_Marine_WDL_Cyborg_ECH252_dp: VES_CH252_MAR_Vacuum_dp
-	{
-		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (WDL)[OFF]";
-		CTAB_Camera="true";
-		hiddenSelectionsTextures[]=
-		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_WDL.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_ard_CO.paa",
+			""
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_WDL.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_ard_CO.paa",
+				""
 			};
 		};
 	};
-	class 89th_Marine_URB_Cyborg_ECH252: VES_CH252_MAR_Vacuum
+	class 89th_Marine_DES_Cyborg_CH252: VES_CH252_MAR_Light
 	{
 		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (URB)";
-		model="\optre_unsc_units\army\helmet";
 		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Cyborg)(DES)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_URB.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
-		};
-		class ItemInfo: ItemInfo
-		{
-			model="\optre_unsc_units\army\helmet";
-			hiddenSelectionsTextures[]=
-			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_URB.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
-			};
-		};
-	};
-	class 89th_Marine_URB_Cyborg_ECH252_dp: VES_CH252_MAR_Vacuum_dp
-	{
-		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (URB)[OFF]";
-		CTAB_Camera="true";
-		hiddenSelectionsTextures[]=
-		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_URB.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_des_CO.paa",
+			""
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_URB.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_des_CO.paa",
+				""
 			};
 		};
 	};
-	class 89th_Marine_SNW_Cyborg_ECH252: VES_CH252_MAR_Vacuum
+	class 89th_Marine_SNW_Cyborg_CH252: VES_CH252_MAR_Light
 	{
 		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (SNW)";
-		model="\optre_unsc_units\army\helmet";
 		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Cyborg)(SNW)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_SNW.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
-		};
-		class ItemInfo: ItemInfo
-		{
-			model="\optre_unsc_units\army\helmet";
-			hiddenSelectionsTextures[]=
-			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_SNW.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
-			};
-		};
-	};
-	class 89th_Marine_SNW_Cyborg_ECH252_dp: VES_CH252_MAR_Vacuum_dp
-	{
-		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (SNW)[OFF]";
-		CTAB_Camera="true";
-		hiddenSelectionsTextures[]=
-		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_SNW.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_snw_CO.paa",
+			""
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_SNW.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_snw_CO.paa",
+				""
 			};
 		};
 	};
-		class 89th_Marine_ARD_Cyborg_ECH252: VES_CH252_MAR_Vacuum
+	class 89th_Marine_URB_Cyborg_CH252: VES_CH252_MAR_Light
 	{
 		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (ARD)";
-		model="\optre_unsc_units\army\helmet";
 		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Cyborg)(URB)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_ARD.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
-		};
-		class ItemInfo: ItemInfo
-		{
-			model="\optre_unsc_units\army\helmet";
-			hiddenSelectionsTextures[]=
-			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_ARD.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
-			};
-		};
-	};
-	class 89th_Marine_ARD_Cyborg_ECH252_dp: VES_CH252_MAR_Vacuum_dp
-	{
-		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (ARD)[OFF]";
-		CTAB_Camera="true";
-		hiddenSelectionsTextures[]=
-		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_ARD.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_urb_CO.paa",
+			""
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_ARD.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_urb_CO.paa",
+				""
 			};
 		};
 	};
-	class 89th_Marine_DES_Cyborg_ECH252: VES_CH252_MAR_Vacuum
+	class 89th_Marine_WDL_Cyborg_CH252: VES_CH252_MAR_Light
 	{
 		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (DES)";
-		model="\optre_unsc_units\army\helmet";
 		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Cyborg)(WDL)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_DES.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
-		};
-		class ItemInfo: ItemInfo
-		{
-			model="\optre_unsc_units\army\helmet";
-			hiddenSelectionsTextures[]=
-			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_DES.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
-			};
-		};
-	};
-	class 89th_Marine_DES_Cyborg_ECH252_dp: VES_CH252_MAR_Vacuum_dp
-	{
-		dlc="89thJTF";
-		author="Tas, Jake, Shelly, and Vespade";
-		displayName="[89th] Cyborg CH252 Helmet (DES)[OFF]";
-		CTAB_Camera="true";
-		hiddenSelectionsTextures[]=
-		{
-			"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_DES.paa",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_wdl_CO.paa",
+			""
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Armor\Data\Custom\Cyborg\89th_marine_helmet_Cyborg_DES.paa",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Armor\Data\Custom\Cyborg\89th_Cyborg_wdl_CO.paa",
+				""
 			};
 		};
 	};
