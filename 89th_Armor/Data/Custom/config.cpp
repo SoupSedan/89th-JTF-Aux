f857dea4d4c09f5ef1652f5b0404af692bebb7c5
Flipoholic <83806159+FlipperG@users.noreply.github.com>
2022-05-31 04:20:36 -0500

    Grim & Flips Vests added

--- 89th_Armor/Data/Custom/config.cpp
+++ 89th_Armor/Data/Custom/config.cpp
@@ -1195,6 +1195,294 @@ class cfgWeapons
 			"89th_Armor\Data\Custom\Mark\89th_M52A_MARK_WDL.paa"
 		};
 	};
+	//Flips Vest
+	class 89th_M52A_Flips_ARD: VES_M52A_MAR_Rifleman_C
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Flips) (ARD)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\ARD\89th_Soft_Vest_ARD.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52_Flips_ARD.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52_ARD_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52D_Flips_ARD.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			mass=75;
+			containerClass="Supply250";
+			uniformModel="\OPTRE_UNSC_Units\Army\armor";
+			vestType="Rebreather";
+			class HitpointsProtectionInfo
+			{
+				class Abdomen {armor=35; hitpointName="HitAbdomen"; passThrough=0.1;};
+				class Arms: Abdomen {hitpointName="HitArms";};
+				class Body: Abdomen {hitpointName="HitBody";};
+				class Chest: Abdomen {hitpointName="HitChest";};
+				class Diaphragm: Abdomen {hitpointName="HitDiaphragm";};
+				class Hands: Abdomen {hitpointName="HitHands";};
+				class Legs: Abdomen {hitpointName="HitLegs";};
+				class Neck: Abdomen {hitpointName="HitNeck";};
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+			\\	"A_ChestArmor",
+			\\	"A_KneesLeft",
+			\\	"A_KneesRight",
+				"A_KneesMarLeft",
+			\\	"A_KneesMarRight",
+				"A_ODST",
+				"A_ShinArmorLeft",
+			\\	"A_ShinArmorRight",
+				"A_TacPad",
+			\\	"A_ThighArmorLeft",
+			\\	"A_ThighArmorRight",
+			\\	"AS_BaseLeft",
+			\\	"AS_BaseRight",
+			\\	"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+				"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+			\\	"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+			\\	"AP_Canteen",
+				"AP_GL",
+				"AP_Knife",
+				"AP_MGThigh",
+				"AP_AR",
+				"AP_BR",
+				"AP_Pack",
+			\\	"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+			\\	"AP_Sniper",
+				"AP_Thigh",
+				"AP_Frag",
+			\\	"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+			\\	"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};	
+	class 89th_M52A_Flips_DES: 89th_M52A_Flips_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Flips) (DES)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\DES\89th_Soft_Vest_DES.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52_Flips_DES.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52_DES_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52D_Flips_ARD.paa"
+		};
+	};
+	class 89th_M52A_Flips_SNW: 89th_M52A_Flips_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Flips) (SNW)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\SNW\89th_Soft_Vest_SNW.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52_Flips_SNW.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52_SNW_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52D_Flips_SNW.paa"
+		};
+	};
+	class 89th_M52A_Flips_URB: 89th_M52A_Flips_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Flips) (URB)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\URB\89th_Soft_Vest_URB.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52_Flips_URB.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52_URB_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52D_Flips_URB.paa"
+		};
+	};
+	class 89th_M52A_Flips_WDL: 89th_M52A_Flips_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Flips) (WDL)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\WDL\89th_Soft_Vest_WDL.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52_Flips_WDL.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52_WDL_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Custom\Flips\89th_M52D_Flips_WDL.paa"
+		};
+	};
+	//Grim Vest
+	class 89th_M52A_Grim_ARD: 89th_M52A_TeamLeader
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Grim) (ARD)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\ARD\89th_Soft_Vest_ARD.paa",
+			"89th_Armor\Data\Custom\Grim\89th_M52_Grim_ARD.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52_ARD_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52D_ARD_CO.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			mass=75;
+			containerClass="Supply250";
+			uniformModel="\OPTRE_UNSC_Units\Army\armor";
+			vestType="Rebreather";
+			class HitpointsProtectionInfo
+			{
+				class Abdomen {armor=35; hitpointName="HitAbdomen"; passThrough=0.1;};
+				class Arms: Abdomen {hitpointName="HitArms";};
+				class Body: Abdomen {hitpointName="HitBody";};
+				class Chest: Abdomen {hitpointName="HitChest";};
+				class Diaphragm: Abdomen {hitpointName="HitDiaphragm";};
+				class Hands: Abdomen {hitpointName="HitHands";};
+				class Legs: Abdomen {hitpointName="HitLegs";};
+				class Neck: Abdomen {hitpointName="HitNeck";};
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+			\\	"A_ChestArmor",
+			\\	"A_KneesLeft",
+				"A_KneesRight",
+			\\	"A_KneesMarLeft",
+			\\	"A_KneesMarRight",
+				"A_ODST",
+			\\	"A_ShinArmorLeft",
+			\\	"A_ShinArmorRight",
+				"A_TacPad",
+			\\	"A_ThighArmorLeft",
+			\\	"A_ThighArmorRight",
+			\\	"AS_BaseLeft",
+			\\	"AS_BaseRight",
+			\\	"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+			\\	"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+				"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+			\\	"AP_Canteen",
+				"AP_GL",
+			\\	"AP_Knife",
+				"AP_MGThigh",
+			\\	"AP_AR",
+				"AP_BR",
+			\\	"AP_Pack",
+				"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+			\\	"AP_Sniper",
+				"AP_Thigh",
+				"AP_Frag",
+			\\	"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};	
+	class 89th_M52A_Grim_DES: 89th_M52A_Grim_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Grim) (DES)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\DES\89th_Soft_Vest_DES.paa",
+			"89th_Armor\Data\Custom\Grim\89th_M52_Grim_DES.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52_DES_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52D_DES_CO.paa"
+		};
+	};
+	class 89th_M52A_Grim_SNW: 89th_M52A_Grim_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Grim) (SNW)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\SNW\89th_Soft_Vest_SNW.paa",
+			"89th_Armor\Data\Custom\Grim\89th_M52_Grim_SNW.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52_SNW_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52D_SNW_CO.paa"
+		};
+	};
+	class 89th_M52A_Grim_URB: 89th_M52A_Grim_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Grim) (URB)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\URB\89th_Soft_Vest_URB.paa",
+			"89th_Armor\Data\Custom\Grim\89th_M52_Grim_URB.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52_URB_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52D_URB_CO.paa"
+		};
+	};
+	class 89th_M52A_Grim_WDL: 89th_M52A_Grim_ARD
+	{
+		dlc="89thJTF";
+		author="Flips";
+		displayName="[89th] M52A (Grim) (WDL)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\WDL\89th_Soft_Vest_WDL.paa",
+			"89th_Armor\Data\Custom\Grim\89th_M52_Grim_WDL.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52_WDL_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52D_WDL_CO.paa"
+		};
+	};
 	//Grim Vest
 	class 89th_M52A_Grim_ARD: VES_M52A_MAR_Rifleman_C
 	{
