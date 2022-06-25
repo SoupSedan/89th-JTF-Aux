618ac76d9049049d61df822d63b11eea89f4b110
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2022-06-25 10:41:48 -1000

    Chrysler helmet

--- 89th_Armor/Data/Custom/config.cpp
+++ 89th_Armor/Data/Custom/config.cpp
@@ -4939,6 +4939,218 @@ class 89th_M52_Tas: VES_M52D_Demolitions
 			};
 		};
 	};
+		//Chrysler Helmet
+	class 89th_Marine_Chrysler_WDL_CH252: VES_CH252_MAR_Vacuum
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Chrysler)(WDL)";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_WDL_CO.paa",
+			"89th_Armor\Data\Visors\89th_Marine_V_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_WDL_CO.paa",
+				""
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_WDL_CH252_dp: VES_CH252_MAR_Vacuum_dp
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="CH252 (Chrysler)(WDL)[OFF]";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_SChrysler_WDL_CO.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_WDL_CO.paa",
+				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_URB_CH252: VES_CH252_MAR_Vacuum
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Chrysler)(URB)";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_URB_CO.paa",
+			"89th_Armor\Data\Visors\89th_Marine_V_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_URB_CO.paa",
+				""
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_URB_CH252_dp: VES_CH252_MAR_Vacuum_dp
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="CH252 (Chrysler)(URB)[OFF]";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_URB_CO.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_URB_CO.paa",
+				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_DES_CH252: VES_CH252_MAR_Vacuum
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Chrysler)(DES)";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_DES_CO.paa",
+			"89th_Armor\Data\Visors\89th_Marine_V_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_DES_CO.paa",
+				""
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_DES_CH252_dp: VES_CH252_MAR_Vacuum_dp
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="CH252 (Chrysler)(DES)[OFF]";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_DES_CO.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_DES_CO.paa",
+				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_ARD_CH252: VES_CH252_MAR_Vacuum
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Chrysler)(ARD)";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_ARD_CO.paa",
+			"89th_Armor\Data\Visors\89th_Marine_V_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_ARD_CO.paa",
+				""
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_ARD_CH252_dp: VES_CH252_MAR_Vacuum_dp
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="CH252 (Chrysler)(ARD)[OFF]";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_ARD_CO.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_ARD_CO.paa",
+				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			};
+		};
+	};
+
+	class 89th_Marine_Chrysler_SNW_CH252: VES_CH252_MAR_Vacuum
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="[89th] CH252 (Chrysler)(SNW)";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_SNW_CO.paa",
+			"89th_Armor\Data\Visors\89th_Marine_V_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_SNW_CO.paa",
+				""
+			};
+		};
+	};
+	class 89th_Marine_Chrysler_SNW_CH252_dp: VES_CH252_MAR_Vacuum_dp
+	{
+		dlc="89thJTF";
+		scope=2;
+		author="Soup";
+		displayName="CH252 (Chrysler)(SNW)[OFF]";
+		CTAB_Camera="true";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_SNW_CO.paa",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelectionsTextures[]=
+			{
+				"89th_Armor\Data\Custom\Chrysler\89th_marine_helmet_Chrysler_SNW_CO.paa",
+				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			};
+		};
+	};
 	//Fenn Helmet
 	 class 89th_Marine_Fenn_WDL_CH252: VES_CH252_MAR_Vacuum
 	{
