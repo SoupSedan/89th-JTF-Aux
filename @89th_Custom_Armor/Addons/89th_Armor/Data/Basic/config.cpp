a3ca6861380a409016477d742fcf054f054a7447
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2022-09-11 10:55:32 -1000

    CLS coding

--- @89th_Custom_Armor/Addons/89th_Armor/Data/Basic/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Armor/Data/Basic/config.cpp
@@ -954,6 +954,166 @@ class cfgWeapons
 			vestType="Rebreather";
 		};
 	};
+	//CLS Vest
+	class 89th_M52A_CLS: VES_M52A_MAR_Rifleman_C
+	{
+		dlc="89thJTF";
+		author="Tas, Geek, and Vespade";
+		displayName="[89th] M52A (CLS)(ARD)";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Basic\ARD\89th_Soft_Vest_ARD.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52_CLS_ARD_CO.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52_ARD_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_desert_co.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52D_ARD_CO.paa"
+		};
+		class ItemInfo: VestItem
+		{
+			mass=75;
+			containerClass="Supply250";
+			uniformModel="\OPTRE_UNSC_Units\Army\armor";
+			vestType="Rebreather";
+			class HitpointsProtectionInfo
+			{
+				class Abdomen {armor=35; hitpointName="HitAbdomen"; passThrough=0.1;};
+				class Arms: Abdomen {hitpointName="HitArms";};
+				class Body: Abdomen {hitpointName="HitBody";};
+				class Chest: Abdomen {hitpointName="HitChest";};
+				class Diaphragm: Abdomen {hitpointName="HitDiaphragm";};
+				class Hands: Abdomen {hitpointName="HitHands";};
+				class Legs: Abdomen {hitpointName="HitLegs";};
+				class Neck: Abdomen {hitpointName="HitNeck";};
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+			\\	"A_ChestArmor",
+			\\	"A_KneesLeft",
+				"A_KneesRight",
+			\\	"A_KneesMarLeft",
+			\\	"A_KneesMarRight",
+				"A_ODST",
+			\\	"A_ShinArmorLeft",
+			\\	"A_ShinArmorRight",
+				"A_TacPad",
+			\\	"A_ThighArmorLeft",
+			\\	"A_ThighArmorRight",
+			\\	"AS_BaseLeft",
+			\\	"AS_BaseRight",
+			\\	"AS_LargeLeft",
+				"AS_LargeRight",
+				"AS_MediumLeft",
+			\\	"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+				"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+				"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+				"AP_Canteen",
+				"AP_GL",
+			\\	"AP_Knife",
+				"AP_MGThigh",
+				"AP_AR",
+				"AP_BR",
+				"AP_Pack",
+				"AP_Pistol",
+				"AP_Rounds",
+			\\	"AP_SG",
+			\\	"AP_SMG",
+				"AP_Sniper",
+			\\	"AP_Thigh",
+			\\	"AP_Frag",
+			\\	"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};
+	class 89th_Marine_B_M52A_URB_CLS: VES_M52A_MAR_Corpsman_A
+	{
+		dlc="89thJTF";
+		author="Tas, Geek, and Vespade";
+		displayName="[89th] M52A (CLS)(URB)";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Basic\URB\89th_Soft_Vest_URB.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52_CLS_URB_CO.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52_URB_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52D_URB_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			vestType="Rebreather";
+		};
+	};
+	class 89th_Marine_B_M52A_SNW_CLS: VES_M52A_MAR_Corpsman_A
+	{
+		dlc="89thJTF";
+		author="Tas, Geek, and Vespade";
+		displayName="[89th] M52A (CLS)(SNW)";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Basic\SNW\89th_Soft_Vest_SNW.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52_CLS_SNW_CO.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52_SNW_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52D_SNW_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			vestType="Rebreather";
+		};
+	};
+	class 89th_Marine_B_M52A_DES_CLS: VES_M52A_MAR_Corpsman_A
+	{
+		dlc="89thJTF";
+		author="Tas, Geek, and Vespade";
+		displayName="[89th] M52A (CLS)(DES)";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Basic\DES\89th_Soft_Vest_DES.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52_CLS_DES_CO.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52_DES_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_desert_co.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52D_DES_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			vestType="Rebreather";
+		};
+	}
+	class 89th_Marine_B_M52A_WDL_CLS: VES_M52A_MAR_Corpsman_A
+	{
+		dlc="89thJTF";
+		author="Tas, Geek, and Vespade";
+		displayName="[89th] M52A (CLS)(WDL)";
+		hiddenSelectionsTextures[]=
+		{
+			"89th_Armor\Data\Basic\WDL\89th_Soft_Vest_WDL.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52_CLS_WDL_CO.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52_WDL_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_desert_co.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52D_WDL_CO.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			vestType="Rebreather";
+		};
+	}
 		//Medic Vest
 	class 89th_Marine_B_M52A_URB: VES_M52A_MAR_Corpsman_A
 	{
