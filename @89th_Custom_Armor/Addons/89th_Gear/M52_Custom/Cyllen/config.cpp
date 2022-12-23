b512cfcadf72986a70705349bbbdba9498918ee8
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2022-12-22 19:58:30 -0500

    Cyllen gets a vest

---
+++ @89th_Custom_Armor/Addons/89th_Gear/M52_Custom/Cyllen/config.cpp
@@ -0,0 +1,255 @@
+class cfgPatches
+{
+	class 89th_M52A_Cyllen
+	{
+		author="89th Mod Team";
+		addonRootClass="89th_M52_patches";
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"89th_M52_patches"
+		};
+		weapons[]=
+		{
+			"89th_M52A_Cyllen_ARD",
+			"89th_M52A_Cyllen_DES",
+			"89th_M52A_Cyllen_SNW",
+			"89th_M52A_Cyllen_URB",
+			"89th_M52A_Cyllen_WDL"
+		};
+	};
+};
+class cfgWeapons
+{
+	class 89th_M52A_Rifleman;
+	class ItemInfo;
+	class VestItem;
+	class 89th_M52_Cyllen: VES_M52D_Rifleman
+	{
+		dlc="FZ";
+		author="Vice";
+		displayName="[89th] M52A (Cyllen) (WDL)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\WDL\89th_Soft_Vest_WDL.paa",
+			"89th_Armor\Data\Custom\Cyllen\89th_M52A_Cyllen_WDL.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52_WDL_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\WDL\89th_M52D_WDL_CO.paa"};
+		class ItemInfo: VestItem
+		{
+			mass=75;
+			containerClass="Supply250";
+			uniformModel="\OPTRE_UNSC_Units\Army\armor";
+			vestType="Rebreather";
+			class HitpointsProtectionInfo
+			{
+				class Abdomen {armor=35; hitpointName="HitAbdomen"; passThrough=0.1;};
+				class Arms: Abdomen {hitpointName="HitArms";};
+				class Body: Abdomen {hitpointName="HitBody";};
+				class Chest: Abdomen {hitpointName="HitChest";};
+				class Diaphragm: Abdomen {hitpointName="HitDiaphragm";};
+				class Hands: Abdomen {hitpointName="HitHands";};
+				class Legs: Abdomen {hitpointName="HitLegs";};
+				class Neck: Abdomen {hitpointName="HitNeck";};
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"A_Ghillie",
+			\\	"A_ChestArmor",
+			\\	"A_KneesLeft",
+				"A_KneesRight",
+			\\	"A_KneesMarLeft",
+			\\	"A_KneesMarRight",
+				"A_ODST",
+			\\	"A_ShinArmorLeft",
+			\\	"A_ShinArmorRight",
+				"A_TacPad",
+			\\	"A_ThighArmorLeft",
+			\\	"A_ThighArmorRight",
+			\\	"AS_BaseLeft",
+			\\	"AS_BaseRight",
+				"AS_LargeLeft",
+				"AS_LargeRight",
+			\\	"AS_MediumLeft",
+				"AS_MediumRight",
+				"AS_ODSTCQBLeft",
+				"AS_ODSTCQBRight",
+				"AS_ODSTLeft",
+			\\	"AS_ODSTRight",
+				"AS_ODSTSniperLeft",
+				"AS_ODSTSniperRight",
+				"AS_SmallLeft",
+				"AS_SmallRight",
+			\\	"AP_Canteen",
+				"AP_GL",
+			\\	"AP_Knife",
+				"AP_MGThigh",
+			\\	"AP_AR",
+				"AP_BR",
+				"AP_Pack",
+				"AP_Pistol",
+				"AP_Rounds",
+				"AP_SG",
+				"AP_SMG",
+			\\	"AP_Sniper",
+				"AP_Thigh",
+				"AP_Frag",
+			\\	"AP_Smoke",
+				"APO_AR",
+				"APO_BR",
+				"APO_Knife",
+				"APO_SMG",
+				"APO_Sniper",
+				"CustomKit_Scorch"
+			};
+		};
+	};
+	class 89th_M52_Cyllen_DES: 89th_M52_Cyllen
+	{
+		dlc="FZ";
+		author="Vice";
+		displayName="[89th] M52 (Cyllen) (DES)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\DES\89th_Soft_Vest_DES.paa",
+			"89th_Armor\Data\Custom\Cyllen\89th_M52A_Cyllen_DES.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52_DES_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\DES\89th_M52D_DES_CO.paa"
+		};
+	};
+	class 89th_M52_Cyllen_URB: 89th_M52_Cyllen
+	{
+		dlc="FZ";
+		author="Vice";
+		displayName="[89th] M52 (Cyllen) (URB)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\URB\89th_Soft_Vest_URB.paa",
+			"89th_Armor\Data\Custom\Cyllen\89th_M52A_Cyllen_URB.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52_URB_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\URB\89th_M52D_URB_CO.paa"
+		};
+	};
+	class 89th_M52_Cyllen_SNW: 89th_M52_Cyllen
+	{
+		dlc="FZ";
+		author="Vice";
+		displayName="[89th] M52 (Cyllen)) (SNW)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\SNW\89th_Soft_Vest_SNW.paa",
+			"89th_Armor\Data\Custom\Cyllen\89th_M52A_Cyllen_SNW.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52_SNW_UNDERLAY_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\SNW\89th_M52D_SNW_CO.paa"
+		};
+	};
+	class 89th_M52_Cyllen_ARD: 89th_M52_Cyllen
+	{
+		dlc="FZ";
+		author="Vice";
+		displayName="[89th] M52 (Cyllen) (ARD)";
+		hiddenSelectionsTextures[]=
+		{	
+			"89th_Armor\Data\Basic\ARD\89th_Soft_Vest_ARD.paa",
+			"89th_Armor\Data\Custom\Cyllen\89th_M52A_Cyllen_ARD.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52_ARD_Underlay_CO.paa",
+			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
+			"89th_Armor\Data\Basic\ARD\89th_M52D_ARD_CO.paa"
+		};
+	};
+};
+class XtdGearModels
+{
+	class CfgWeapons
+	{
+		class 89th_Cyllen_M52A
+		{
+			label="[89th] Cyllen M52A";
+			author="89th Mod Team";
+			options[]=
+			{
+				"camo"
+			};
+			class camo
+			{
+				label="Camo";
+				alwaysSelectable=1;
+				values[]=
+				{
+					"ARD",
+					"DES",
+					"SNW",
+					"URB",
+					"WDL"
+				};
+				class ARD
+				{
+					label="Arid";
+					image="#(rgb,8,8,3)color(0.278,0.243,0.149,1)";
+				};
+				class DES
+				{
+					label="Desert";
+					image="#(rgb,8,8,3)color(0.431,0.369,0.302,1)";
+				};
+				class SNW
+				{
+					label="Snow";
+					image="#(rgb,8,8,3)color(0.882,0.894,0.902,1)";
+				};
+				class URB
+				{
+					label="Urban";
+					image="#(rgb,8,8,3)color(0.192,0.212,0.259,1)";
+				};
+				class WDL
+				{
+					label="Woodland";
+					image="#(rgb,8,8,3)color(0.118,0.137,0.020,1)";
+				};
+			};
+		};
+	};
+};
+class XtdGearInfos
+{
+	class CfgWeapons
+	{
+		class 89th_M52A_Cyllen_ARD
+		{
+			model="89th_Cyllen_M52A";
+			camo="ARD";
+		};
+		class 89th_M52A_Cyllen_DES
+		{
+			model="89th_Cyllen_M52A";
+			camo="DES";
+		};
+		class 89th_M52A_Cyllen_SNW
+		{
+			model="89th_Cyllen_M52A";
+			camo="SNW";
+		};
+		class 89th_M52A_Cyllen_URB
+		{
+			model="89th_Cyllen_M52A";
+			camo="URB";
+		};
+		class 89th_M52A_Cyllen_WDL
+		{
+			model="89th_Cyllen_M52A";
+			camo="WDL";
+		};
+	};
+};
+
+
