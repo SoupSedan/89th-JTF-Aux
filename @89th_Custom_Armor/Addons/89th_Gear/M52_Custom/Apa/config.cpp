a78f6b478f9e915655ba659f8c6af9cb8406b24c
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2023-02-25 09:38:02 -0500

    Updated Apa's config

--- @89th_Custom_Armor/Addons/89th_Gear/M52_Custom/Apa/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Gear/M52_Custom/Apa/config.cpp
@@ -36,7 +36,7 @@ class cfgWeapons
 			"89th_Gear\M52_Custom\Apa\89th_M52A_Apa_ARD.paa",
 			"89th_Gear\M52_Basic\ARD\89th_M52_Underlay_ARD.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"89th_Gear\M52_Basic\ARD\89th_M52D_ARD.paa"
+			"89th_Gear\M52_Custom\Apa\89th_M52D_Apa_ARD.paa"
 		};
 		class ItemInfo: VestItem
 		{
@@ -89,7 +89,7 @@ class cfgWeapons
 				"AS_SmallLeft",
 				"AS_SmallRight",
 				"AP_Canteen",
-				"AP_GL",
+			\\	"AP_GL",
 			\\	"AP_Knife",
 				"AP_MGThigh",
 				"AP_AR",
@@ -98,7 +98,7 @@ class cfgWeapons
 				"AP_Pistol",
 			\\	"AP_Rounds",
 				"AP_SG",
-			\\	"AP_SMG",
+				"AP_SMG",
 				"AP_Sniper",
 				"AP_Thigh",
 				"AP_Frag",
@@ -123,7 +123,7 @@ class cfgWeapons
 			"89th_Gear\M52_Custom\Apa\89th_M52A_Apa_DES.paa",
 			"89th_Gear\M52_Basic\DES\89th_M52_Underlay_DES.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"89th_Gear\M52_Basic\DES\89th_M52D_DES.paa"
+			"89th_Gear\M52_Custom\Apa\89th_M52D_Apa_DES.paa"
 		};
 	};
 	class 89th_M52A_Apa_SNW: 89th_M52A_Apa_ARD
@@ -137,7 +137,7 @@ class cfgWeapons
 			"89th_Gear\M52_Custom\Apa\89th_M52A_Apa_SNW.paa",
 			"89th_Gear\M52_Basic\SNW\89th_M52_Underlay_SNW.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"89th_Gear\M52_Basic\SNW\89th_M52D_SNW.paa"
+			"89th_Gear\M52_Custom\Apa\89th_M52D_Apa_SNW.paa"
 		};
 	};
 	class 89th_M52A_Apa_URB: 89th_M52A_Apa_ARD
@@ -151,7 +151,7 @@ class cfgWeapons
 			"89th_Gear\M52_Custom\Apa\89th_M52A_Apa_URB.paa",
 			"89th_Gear\M52_Basic\URB\89th_M52_Underlay_URB.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"89th_Gear\M52_Basic\URB\89th_M52D_URB.paa"
+			"89th_Gear\M52_Custom\Apa\89th_M52D_Apa_URB.paa"
 		};
 	};
 	class 89th_M52A_Apa_WDL: 89th_M52A_Apa_ARD
@@ -165,7 +165,7 @@ class cfgWeapons
 			"89th_Gear\M52_Custom\Apa\89th_M52A_Apa_WDL.paa",
 			"89th_Gear\M52_Basic\WDL\89th_M52_Underlay_WDL.paa",
 			"optre_unsc_units\army\data\ghillie_woodland_co.paa",
-			"89th_Gear\M52_Basic\WDL\89th_M52D_WDL.paa"
+			"89th_Gear\M52_Custom\Apa\89th_M52D_Apa_WDL.paa"
 		};
 	};
 };
