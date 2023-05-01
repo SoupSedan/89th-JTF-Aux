3c131278e2c59a613f173b69906777f31d48dd56
Flipoholic <kreepythegamer@gmail.com>
2023-05-01 14:50:22 -0500

    Base Legion Armor Update

--- @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Legion/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Legion/config.cpp
@@ -1,6 +1,6 @@
 class cfgPatches
 {
-	class 89th_CH252A_Custom
+	class OPTRE_FC_VX19_Helmet_Custom
 	{
 		author="89th Mod Team";
 		addonRootClass="89th_CH252_patches";
@@ -12,55 +12,55 @@ class cfgPatches
 		units[] = {};
 		weapons[]=
 		{
-			"89th_CH252A_Legion",
-			"89th_CH252A_Legion_dp"
+			"89th_VX19_Legion",
+			"89th_VX19_Legion_dp"
 		};
 	};
 };
 class cfgWeapons
 {
-	class 89th_CH252A;
-	class 89th_CH252A_dp;
+	class OPTRE_FC_VX19_Helmet;
+	class OPTRE_FC_VX19_Helmet_dp;
 	class ItemInfo;
-    class 89th_CH252A_Legion: 89th_CH252A
+    class 89th_VX19_Legion: OPTRE_FC_VX19_Helmet
 	{
 		dlc="89thJTF";
 		scope=2;
 		author="Soup, Vespade";
-		displayName="[89th] CH252A (Legion)";
+		displayName="[89th] VX19 (Legion)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Legion\89th_CH252A_Legion",
-			"89th_Gear\Visors\89th_Marine_V_CO.paa"
+			"89th_Gear\CH252_Custom\Legion\89th_VX19_Legion.paa",
+			"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Legion\89th_CH252A_Legion",
-				"89th_Gear\Visors\89th_Marine_V_CO.paa"
+				"89th_Gear\CH252_Custom\Legion\89th_VX19_Legion.paa",
+				"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 			};
 		};
 	};
-	class 89th_CH252A_Legion_dp: 89th_CH252A_dp
+	class 89th_VX19_Legion_dp: OPTRE_FC_VX19_Helmet_dp
 	{
 		dlc="89thJTF";
-		scope=2;
+		scope=0;
 		author="Soup, Vespade";
-		displayName="CH252A (Legion)[OFF]";
+		displayName="VX19 (Legion)[OFF]";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Legion\89th_CH252A_Legion",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_gear\CH252_Custom\Legion\89th_VX19_Legion.paa",
+			"89th_gear\CH252_Custom\Legion\h3_pilothelmet_visor_clear_CO.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Legion\89th_CH252A_Legion",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Gear\CH252_Custom\Legion\89th_VX19_Legion.paa",
+				"89th_gear\CH252_Custom\Legion\h3_pilothelmet_visor_clear_CO.paa"
 			};
 		};
 	};
