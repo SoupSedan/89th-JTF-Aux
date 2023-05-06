ead9661819b103d71a911c74ce3f4f005a780249
Flipoholic <kreepythegamer@gmail.com>
2023-05-06 02:29:32 -0500

    Legion VX19 Transition Start

--- @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Battle/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Battle/config.cpp
@@ -1,6 +1,6 @@
 class cfgPatches
 {
-	class 89th_CH252A_Custom
+	class OPTRE_FC_VX19_Helmet_Custom
 	{
 		author="89th Mod Team";
 		addonRootClass="89th_CH252_patches";
@@ -12,55 +12,55 @@ class cfgPatches
 		units[] = {};
 		weapons[]=
 		{
-			"89th_CH252A_Battle",
-			"89th_CH252A_Battle_dp"
+			"89th_VX19_Battle",
+			"89th_VX19_Battle_dp"
 		};
 	};
 };
 class cfgWeapons
 {
-	class 89th_CH252A;
-	class 89th_CH252A_dp;
+	class OPTRE_FC_VX19_Helmet;
+	class OPTRE_FC_VX19_Helmet_dp;
 	class ItemInfo;
-    class 89th_CH252A_Battle: 89th_CH252A
+    class 89th_VX19_Battle: OPTRE_FC_VX19_Helmet
 	{
 		dlc="89thJTF";
 		scope=2;
-		author="Soup, Flips, Vespade";
-		displayName="[89th] CH252A (Battle)";
+		author="Flips";
+		displayName="[89th] VX19 (Battle)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Battle\89th_CH252A_Battle",
-			"89th_Gear\Visors\89th_Marine_V_CO.paa"
+			"89th_Gear\CH252_Custom\Battle\89th_VX19_Battle.paa",
+			"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Battle\89th_CH252A_Battle",
-				"89th_Gear\Visors\89th_Marine_V_CO.paa"
+				"89th_Gear\CH252_Custom\Battle\89th_VX19_Battle.paa",
+				"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 			};
 		};
 	};
-	class 89th_CH252A_Battle_dp: 89th_CH252A_dp
+	class 89th_VX19_Battle_dp: OPTRE_FC_VX19_Helmet_dp
 	{
 		dlc="89thJTF";
-		scope=2;
-		author="Soup, Flips, Vespade";
-		displayName="CH252A (Battle)[OFF]";
+		scope=0;
+		author="Flips";
+		displayName="VX19 (Battle)[OFF]";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Battle\89th_CH252A_Battle",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_gear\CH252_Custom\Battle\89th_VX19_Battle.paa",
+			"89th_gear\CH252_Custom\Battle\h3_pilothelmet_visor_clear_CO.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Battle\89th_CH252A_Battle",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Gear\CH252_Custom\Battle\89th_VX19_Battle.paa",
+				"89th_gear\CH252_Custom\Battle\h3_pilothelmet_visor_clear_CO.paa"
 			};
 		};
 	};
--- @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Buck/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Buck/config.cpp
@@ -1,6 +1,6 @@
 class cfgPatches
 {
-	class 89th_CH252A_Custom
+	class OPTRE_FC_VX19_Helmet_Custom
 	{
 		author="89th Mod Team";
 		addonRootClass="89th_CH252_patches";
@@ -12,55 +12,55 @@ class cfgPatches
 		units[] = {};
 		weapons[]=
 		{
-			"89th_CH252A_Buck",
-			"89th_CH252A_Buck_dp"
+			"89th_VX19_Buck",
+			"89th_VX19_Buck_dp"
 		};
 	};
 };
 class cfgWeapons
 {
-	class 89th_CH252A;
-	class 89th_CH252A_dp;
+	class OPTRE_FC_VX19_Helmet;
+	class OPTRE_FC_VX19_Helmet_dp;
 	class ItemInfo;
-    class 89th_CH252A_Buck: 89th_CH252A
+    class 89th_VX19_Buck: OPTRE_FC_VX19_Helmet
 	{
 		dlc="89thJTF";
 		scope=2;
-		author="Soup, Vespade";
-		displayName="[89th] CH252A (Buck)";
+		author="Flips";
+		displayName="[89th] VX19 (Buck)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Buck\89th_CH252A_Buck",
-			"89th_Gear\Visors\89th_Marine_V_CO.paa"
+			"89th_Gear\CH252_Custom\Buck\89th_VX19_Buck.paa",
+			"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Buck\89th_CH252A_Buck",
-				"89th_Gear\Visors\89th_Marine_V_CO.paa"
+				"89th_Gear\CH252_Custom\Buck\89th_VX19_Buck.paa",
+				"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 			};
 		};
 	};
-	class 89th_CH252A_Buck_dp: 89th_CH252A_dp
+	class 89th_VX19_Buck_dp: OPTRE_FC_VX19_Helmet_dp
 	{
 		dlc="89thJTF";
-		scope=2;
-		author="Soup, Vespade";
-		displayName="CH252A (Buck)[OFF]";
+		scope=0;
+		author="Flips";
+		displayName="VX19 (Buck)[OFF]";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Buck\89th_CH252A_Buck",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_gear\CH252_Custom\Buck\89th_VX19_Buck.paa",
+			"89th_gear\CH252_Custom\Buck\h3_pilothelmet_visor_clear_CO.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Buck\89th_CH252A_Buck",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Gear\CH252_Custom\Buck\89th_VX19_Buck.paa",
+				"89th_gear\CH252_Custom\Buck\h3_pilothelmet_visor_clear_CO.paa"
 			};
 		};
 	};
