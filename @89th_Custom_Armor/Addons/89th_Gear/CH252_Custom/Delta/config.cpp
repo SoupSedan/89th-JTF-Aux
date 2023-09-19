06f5acda9be8a5c2d0e5b66a48b88ecff4b49c5f
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2023-09-19 16:31:38 -0400

    Delta VX19 conversion

--- @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Delta/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Delta/config.cpp
@@ -1,66 +1,67 @@
 class cfgPatches
 {
-	class 89th_CH252A_Custom
+	class OPTRE_FC_VX19_Helmet_Custom
 	{
 		author="89th Mod Team";
 		addonRootClass="89th_CH252_patches";
 		requiredVersion=0.1;
 		requiredAddons[]=
 		{
-			"89th_CH252_patches"
+			"89th_CH252_patches",
+			"OPTRE_FC_Units_Marines"
 		};
 		units[] = {};
 		weapons[]=
 		{
-			"89th_CH252A_Delta",
-			"89th_CH252A_Delta_dp"
+			"89th_VX19_Delta",
+			"89th_VX19_Delta_dp"
 		};
 	};
 };
 class cfgWeapons
 {
-	class 89th_CH252A;
-	class 89th_CH252A_dp;
+	class OPTRE_FC_VX19_Helmet;
+	class OPTRE_FC_VX19_Helmet_dp;
 	class ItemInfo;
-    class 89th_CH252A_Delta: 89th_CH252A
+    class 89th_VX19_Delta: OPTRE_FC_VX19_Helmet
 	{
 		dlc="89thJTF";
 		scope=2;
-		author="Soup, Flips, Vespade";
-		displayName="[89th] CH252A (Delta)";
+		author="Soup";
+		displayName="[89th] VX19 (Delta)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Delta\89th_CH252A_Delta",
-			"89th_Gear\Visors\89th_Marine_V_CO.paa"
+			"89th_Gear\CH252_Custom\Delta\89th_VX19_Delta.paa",
+			"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Delta\89th_CH252A_Delta",
-				"89th_Gear\Visors\89th_Marine_V_CO.paa"
+				"89th_Gear\CH252_Custom\Delta\89th_VX19_Delta.paa",
+				"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
 			};
 		};
 	};
-	class 89th_CH252A_Delta_dp: 89th_CH252A_dp
+	class 89th_VX19_Delta_dp: OPTRE_FC_VX19_Helmet_dp
 	{
 		dlc="89thJTF";
-		scope=2;
-		author="Soup, Flips, Vespade";
-		displayName="CH252A (Delta)[OFF]";
+		scope=0;
+		author="Flips";
+		displayName="VX19 (Delta)[OFF]";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Delta\89th_CH252A_Delta",
-			"optre_unsc_units\army\data\helmet_visor_ca.paa"
+			"89th_gear\CH252_Custom\Delta\89th_VX19_Delta.paa",
+			"89th_gear\CH252_Custom\Delta\h3_pilothelmet_visor_clear_CO.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_gear\CH252_Custom\Delta\89th_CH252A_Delta",
-				"optre_unsc_units\army\data\helmet_visor_ca.paa"
+				"89th_Gear\CH252_Custom\Delta\89th_VX19_Delta.paa",
+				"89th_gear\CH252_Custom\Delta\h3_pilothelmet_visor_clear_CO.paa"
 			};
 		};
 	};
