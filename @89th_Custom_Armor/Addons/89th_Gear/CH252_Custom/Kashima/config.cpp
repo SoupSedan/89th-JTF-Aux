4274caded227438532ada5a4d1d15d424c10b162
SoupSedan <104839304+SoupSedan@users.noreply.github.com>
2024-02-04 22:58:06 -0500

    Add files via upload

--- @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Kashima/config.cpp
+++ @89th_Custom_Armor/Addons/89th_Gear/CH252_Custom/Kashima/config.cpp
@@ -1,67 +1,66 @@
 class cfgPatches
 {
-	class OPTRE_FC_VX19_Helmet_Custom
+	class 89th_CH252A_Custom
 	{
 		author="89th Mod Team";
 		addonRootClass="89th_CH252_patches";
 		requiredVersion=0.1;
 		requiredAddons[]=
 		{
-			"89th_CH252_patches",
-			"OPTRE_FC_Units_Marines"
+			"89th_CH252_patches"
 		};
 		units[] = {};
 		weapons[]=
 		{
-			"89th_VX19_Kashima",
-			"89th_VX19_Kashima_dp"
+			"89th_CH252A_Kashima",
+			"89th_CH252A_Kashima_dp"
 		};
 	};
 };
 class cfgWeapons
 {
-	class OPTRE_FC_VX19_Helmet;
-	class OPTRE_FC_VX19_Helmet_dp;
+	class 89th_CH252A;
+	class 89th_CH252A_dp;
 	class ItemInfo;
-    class 89th_VX19_Kashima: OPTRE_FC_VX19_Helmet
+    class 89th_CH252A_Kashima: 89th_CH252A
 	{
 		dlc="89thJTF";
 		scope=2;
-		author="Jules";
-		displayName="[89th] VX19 (Kashima)";
+		author="Soup, Flips, Vespade";
+		displayName="[89th] CH252A (Kashima)";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_Gear\CH252_Custom\Kashima\89th_VX19_Kashima.paa",
-			"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
+			"89th_gear\CH252_Custom\Kashima\89th_CH252A_Kashima",
+			"89th_Gear\Visors\89th_Marine_V_CO.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Gear\CH252_Custom\Kashima\89th_VX19_Kashima.paa",
-				"optre_fc_units\marines\data\h3_pilothelmet_visor_co.paa"
+				"89th_gear\CH252_Custom\Kashima\89th_CH252A_Kashima",
+				"89th_Gear\Visors\89th_Marine_V_CO.paa"
 			};
 		};
 	};
-	class 89th_VX19_Kashima_dp: OPTRE_FC_VX19_Helmet_dp
+	class 89th_CH252A_Kashima_dp: 89th_CH252A_dp
 	{
 		dlc="89thJTF";
-		scope=0;
-		author="Flips";
-		displayName="VX19 (Kashima)[OFF]";
+		scope=2;
+		author="Soup, Flips, Vespade";
+		displayName="CH252A (Kashima)[OFF]";
 		CTAB_Camera="true";
 		hiddenSelectionsTextures[]=
 		{
-			"89th_gear\CH252_Custom\Kashima\89th_VX19_Kashima.paa",
-			"89th_gear\CH252_Custom\Kashima\h3_pilothelmet_visor_clear_CO.paa"
+			"89th_gear\CH252_Custom\Kashima\89th_CH252A_Kashima",
+			"optre_unsc_units\army\data\helmet_visor_ca.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelectionsTextures[]=
 			{
-				"89th_Gear\CH252_Custom\Kashima\89th_VX19_Kashima.paa",
-				"89th_gear\CH252_Custom\Kashima\h3_pilothelmet_visor_clear_CO.paa"
+				"89th_gear\CH252_Custom\Kashima\89th_CH252A_Kashima",
+				"optre_unsc_units\army\data\helmet_visor_ca.paa"
 			};
 		};
 	};
